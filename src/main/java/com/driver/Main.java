9c13a13954f119decfd6d7eb11b87c9e8621e08b
GaganMittal847 <gaganmittal048@gmail.com>
2023-02-23 11:12:32 +0530

    encapsolution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly rw = new RWOnly();
+    //rw.name="chagan"; //error : 'name' has private
+    //System.out.println(rw.name);
+    rw.setName("Gagan");
+    System.out.println(rw.getName());
+  }
 }
